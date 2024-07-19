13ab62703410ed3ae7f8bff02fddfb98793c1465
ShinasNajeerKoya <shinasnajeerkoya@gmail.com>
2024-07-19 21:32:04 +0530

    s

--- lib/dart_logic_code_practice/longest_common_prefix.dart
+++ lib/dart_logic_code_practice/longest_common_prefix.dart
@@ -10,4 +10,23 @@
 // Initialize the prefix as the first string.
 // Compare the prefix with each string in the list and shorten it if needed.
 
-// s
+// main() {
+//   List<String> words = ["fower", "flow", "flight"];
+//
+//   if (words.isEmpty) "";
+//
+//   String prefix = words[0];
+//
+//   print("one : ${words.length}");
+//
+//   for (int i = 0; i < words.length ; i++) {
+//     print("sss : ${words.length}");
+//     while (words[i].indexOf(prefix) != 0) {
+//       print("sqaqw : ${words[i].indexOf(prefix)}");
+//       prefix = prefix.substring(0, words.length-1);
+//       if (prefix.isEmpty) return "";
+//     }
+//   }
+//
+//   print(prefix);
+// }
