3579a3a3cf59c87f47fcb471b195a607e923c5e6
ShinasNajeerKoya <shinasnajeerkoya@gmail.com>
2024-07-19 21:31:46 +0530

    s

---
+++ lib/dart_logic_code_practice/longest_common_prefix.dart
@@ -0,0 +1,13 @@
+/// Problem: Find the longest common prefix among an array of strings.
+
+/// Tip:
+/// To find the longest common prefix, compare characters of each string one by one until the characters differ.
+
+// Step-by-Step Explanation:
+//
+// Create a function longestCommonPrefix that takes a list of strings as input.
+// If the list is empty, return an empty string.
+// Initialize the prefix as the first string.
+// Compare the prefix with each string in the list and shorten it if needed.
+
+// s
