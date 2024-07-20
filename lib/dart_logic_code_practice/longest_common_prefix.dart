4efd5945ac95633e5881532da699b26cf20cbe74
ShinasNajeerKoya <shinasnajeerkoya@gmail.com>
2024-07-20 12:48:02 +0530

    longest common prefix done

--- lib/dart_logic_code_practice/longest_common_prefix.dart
+++ lib/dart_logic_code_practice/longest_common_prefix.dart
@@ -10,23 +10,30 @@
 // Initialize the prefix as the first string.
 // Compare the prefix with each string in the list and shorten it if needed.
 
-// main() {
-//   List<String> words = ["fower", "flow", "flight"];
-//
-//   if (words.isEmpty) "";
-//
-//   String prefix = words[0];
-//
-//   print("one : ${words.length}");
-//
-//   for (int i = 0; i < words.length ; i++) {
-//     print("sss : ${words.length}");
-//     while (words[i].indexOf(prefix) != 0) {
-//       print("sqaqw : ${words[i].indexOf(prefix)}");
-//       prefix = prefix.substring(0, words.length-1);
-//       if (prefix.isEmpty) return "";
-//     }
-//   }
-//
-//   print(prefix);
-// }
+main() {
+  List<String> wordsList = [
+    "flower",
+    "flow",
+    "flowers",
+    "flour",
+  ];
+
+  wordsList.sort(); //first sort the list into ascending order
+
+  List<String> firstWordList = wordsList[0].split(""); //split the string into list of letters - Eg:[f,l,o,u,r]
+  List<String> lastWordList = wordsList[wordsList.length - 1].split(""); //Eg: [f,l,o,w,e,r,s]
+
+  String result = ""; // String to store the result
+  
+  ///now we can compare each letters of first and last words letterList to find common prefixes using index
+
+  for (int i = 0; i < firstWordList.length; i++) {    //for loop for each letter of first word
+    if (firstWordList[i] == lastWordList[i]) {        //check each index of the lettersList, if the letters are same,
+      result += firstWordList[i];                     //then save in results and loop again
+    } else {
+      print("Result: $result");                //if first and last words index value not same then come out of the loop
+      print("No more Prefix matches");         // print results and No Matches
+      return;
+    }
+  }
+}
